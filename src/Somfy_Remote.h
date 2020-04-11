6714cfe15885a11a70eee6e194c2efd3ec5bc5a1
Jeff Kowalski <jeff.kowalski@gmail.com>
2020-04-11 12:48:36 -0700

    improve readbility, support esp8266

--- src/Somfy_Remote.h
+++ src/Somfy_Remote.h
@@ -5,26 +5,25 @@ This library is based on the Arduino sketch by Nickduino: https://github.com/Nic
 #ifndef SOMFY_REMOTE
 #define SOMFY_REMOTE
 
-#include <EEPROM.h>
-#include <ELECHOUSE_CC1101_SRC_DRV.h>
+#include <Arduino.h>
 
-class SomfyRemote
-{
-private:
-  String _name;
-  uint32_t _remoteCode;
-  uint32_t _rollingCode;
-  uint16_t _eepromAddress;
+class SomfyRemote {
+  private:
+    String   _name;
+    uint32_t _remoteCode;
+    uint32_t _rollingCode;
+    uint16_t _eepromAddress;
 
-  void buildFrame(uint8_t *frame, uint8_t command);
-  void sendCommand(uint8_t *frame, uint8_t sync);
-  void sendBit(bool value);
-  uint16_t getNextEepromAddress();
-  uint32_t getRollingCode();
+    void     buildFrame(uint8_t * frame, uint8_t command);
+    void     sendCommand(uint8_t * frame, uint8_t sync);
+    void     sendBit(bool value);
+    uint16_t getNextEepromAddress();
+    uint32_t getRollingCode();
 
-public:
-  SomfyRemote(String name, uint32_t remoteCode); // Constructor requires name and remote code
-  String getName();                              // Getter for name
-  void move(String command);                     // Method to send a command (Possible inputs: UP, DOWN, MY, PROGRAM)
+  public:
+    SomfyRemote(String name, uint32_t remoteCode);  // Constructor requires name and remote code
+    String getName();                               // Getter for name
+    void   move(String command);                    // Method to send a command (Possible inputs: UP, DOWN, MY, PROGRAM)
 };
-#endif
+
+#endif  // SOMFY_REMOTE
