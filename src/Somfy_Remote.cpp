9684f1ce5e6d87a515e2023141970de0f302a97c
Jeff Kowalski <jeff.kowalski@gmail.com>
2020-06-21 18:17:40 -0700

    major restructuring
    1. Reintroduce CC1101; this library will work with either the CC1101
    or the FS1000A, based on a definition at the top of Somfy_Rempte.cpp
    2. Introduce a FREQ_OFFSET, to allow for empirically adjustable
    correction to 433.42MHz
    3. Move EEPROM initialization to the constructor, which will allow
    correct operation if the class is statically instantiated in some
    parent code.
    4. Align pulse timings with those found in documentation wherever
    possible.
    5. Optimize data transmission code to eliminate unnecessary
    calculations, allowing pulses to best align to nominal timing.

--- src/Somfy_Remote.cpp
+++ src/Somfy_Remote.cpp
@@ -6,24 +6,41 @@
 #include "Somfy_Remote.h"
 #include <EEPROM.h>
 #include <assert.h>
+#define CC1101  // comment this out to run FS1000A instead
+#ifdef CC1101
+#include <ELECHOUSE_CC1101_SRC_DRV.h>
+#define FREQ_OFFSET (-0.055)  // correction to align to 433,42MHz
+#endif
 
-#define FRAME_LENGTH 7
-static int const transmitPin = D2;
+#define EEPROM_SIZE 64
+#define FRAME_LENGTH 7   // size of frame in bytes
+#define TRANSMIT_PIN D2  // ESP8266 pin to CC1101 GD2
 
 
-// Constructor
 SomfyRemote::SomfyRemote (String name, uint32_t remoteCode)
     : _name (name)
     , _remoteCode (remoteCode) {
     static uint8_t nextEepromAddress = 0;
 
-    _eepromAddress = nextEepromAddress;
-    nextEepromAddress += sizeof (_rollingCode);
-    if (EEPROM.get (_eepromAddress, _rollingCode) < 1)
-        _rollingCode = 1;
+    EEPROM.begin (EEPROM_SIZE);
 
-    pinMode (transmitPin, OUTPUT);
-    digitalWrite (transmitPin, LOW);
+    _eepromAddress = nextEepromAddress;
+    EEPROM.get (_eepromAddress, _rollingCode);
+    nextEepromAddress += sizeof (_rollingCode);
+
+#ifdef CC1101
+    // Choose pins before initializing radio
+    ELECHOUSE_cc1101.setGDO (D1, TRANSMIT_PIN);
+
+    // Initialize radio chip
+    ELECHOUSE_cc1101.Init ();
+
+    // Enable transmission at 433.42 MHz
+    ELECHOUSE_cc1101.SetTx (433.42 + FREQ_OFFSET);
+#endif
+
+    pinMode (TRANSMIT_PIN, OUTPUT);
+    digitalWrite (TRANSMIT_PIN, LOW);
 }
 
 
@@ -41,7 +58,9 @@ static void printFrame (char const * message, uint8_t const * frame) {
 
 // Build frame according to Somfy RTS protocol
 void SomfyRemote::buildFrame (uint8_t * frame, uint8_t command) {
-    frame[0] = 0xA7;               // Encryption key
+    uint8_t key = (_rollingCode - 8) & 0x0F;
+
+    frame[0] = 0xA0 | key;         // Encryption key
     frame[1] = command << 4;       // Selected command in 4 MSB; the 4 LSB are the checksum
     frame[2] = _rollingCode >> 8;  // Rolling code (big endian)
     frame[3] = _rollingCode;       // Rolling code
@@ -54,7 +73,7 @@ void SomfyRemote::buildFrame (uint8_t * frame, uint8_t command) {
     uint8_t checksum = 0;
     for (int ii = 0; ii < FRAME_LENGTH; ++ii)
         checksum = checksum ^ frame[ii] ^ (frame[ii] >> 4);
-    checksum &= 0b00001111;  // Keep the last 4 bits only
+    checksum &= 0x0F;  // Keep the last 4 bits only
     // Checksum integration
     frame[1] |= checksum;  // If XOR of all the nibbles is 0, the blinds will consider the checksum ok
     printFrame ("  checksummed: ", frame);
@@ -71,9 +90,9 @@ void SomfyRemote::buildFrame (uint8_t * frame, uint8_t command) {
 
 
 inline void pulse (int level1, int duration1, int level2, int duration2) {
-    digitalWrite (transmitPin, level1);
+    digitalWrite (TRANSMIT_PIN, level1);
     delayMicroseconds (duration1);
-    digitalWrite (transmitPin, level2);
+    digitalWrite (TRANSMIT_PIN, level2);
     delayMicroseconds (duration2);
 }
 
@@ -82,42 +101,43 @@ inline void pulse (int level1, int duration1, int level2, int duration2) {
 void SomfyRemote::sendCommand (uint8_t const * frame, uint8_t sync) const {
     assert (sync == 2 || sync == 7);  // 2 for first frame, or 7 subsequently
 
-#define SYMBOL 604
+#define SYMBOL 640
 
-    // Only with the first frame: wake-up pulse & silence
+    // Wake-up pulse & silence, only with the first frame
     if (sync == 2)
-        pulse (HIGH, 9415, LOW, 89565);
+        pulse (HIGH, 9986, LOW, 97087);  //  (empirically determined)
 
-    // Hardware sync: 2 sync for the first frame, 7 for the following ones
+    // Pre-sync + Sync = 15.44ms
+
+    // Hardware pre-sync: 2 sync for the first frame, 7 for the following ones
     for (int ii = 0; ii < sync; ++ii)
-        pulse (HIGH, 4 * SYMBOL, LOW, 4 * SYMBOL);
+        pulse (HIGH, 2750, LOW, 2250);
 
     // Software sync
-    pulse (HIGH, 4550, LOW, SYMBOL);
+    pulse (HIGH, 4800, LOW, SYMBOL);  // sync pulse, delimiter
 
-#define BITS 8
-    // Data: bits are sent one by one, starting with the MSB
-    for (int ii = 0; ii < FRAME_LENGTH * BITS; ++ii)
-        // Somfy RTS bits are manchester encoded: 0 = high->low 1 = low->high
-        if (((frame[ii / BITS] >> ((BITS - 1) - (ii % BITS))) & 1) == 1)
-            pulse (LOW, SYMBOL, HIGH, SYMBOL);
-        else
-            pulse (HIGH, SYMBOL, LOW, SYMBOL);
+    // Data: bits are sent one by one, starting with the MSB; 71.68ms
+    for (uint8_t const * fb = frame; fb < &frame[FRAME_LENGTH]; ++fb)  // walk frame bytes
+        for (uint8_t bit = 0b10000000; bit > 0; bit >>= 1)             // walk the 8 bits
+            // Somfy RTS bits are MSB first, manchester encoded: 0 = high->low 1 = low->high
+            if (*fb & bit)
+                pulse (LOW, SYMBOL, HIGH, SYMBOL);
+            else
+                pulse (HIGH, SYMBOL, LOW, SYMBOL);
 
-    digitalWrite (transmitPin, LOW);
-    delayMicroseconds (30415);  // Inter-frame silence
+    digitalWrite (TRANSMIT_PIN, LOW);
+    delayMicroseconds (32286);  // Inter-frame silence (empirically determined)
 }
 
 
 // Send a command to the blinds
 void SomfyRemote::move (String command) {
+    // Build frame according to selected command
     const uint8_t my   = 0x1;
     const uint8_t up   = 0x2;
     const uint8_t down = 0x4;
     const uint8_t prog = 0x8;
-
-    // Build frame according to selected command
-    uint8_t frame[FRAME_LENGTH];
+    uint8_t       frame[FRAME_LENGTH];
 
     command.toUpperCase ();
     switch (command[0]) {
